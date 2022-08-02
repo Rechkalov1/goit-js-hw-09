2327370f3e48ce9684b7cf76ab9a3af5e504d33d
EnVy <envy7321@gmail.com>
2022-08-02 16:48:22 +0300

    save

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,1 +1,87 @@
+// Ипортирую библиотеке
 import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+form=document.querySelector('#datetime-picker');
+timeElAll=document.querySelectorAll('.value');
+buttonStart=document.querySelector("[data-start]")
+// Стучусь к форме и датах
+
+buttonStart.addEventListener('click', onButton);
+buttonStart.disabled = true;
+// параметр flatpickr`a
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose, 
+   
+  
+};
+
+flatpickr(form, options);
+// отсчет времени
+let timer =0;
+let intervalId =null;
+
+function onClose(selectedDates){
+  const newDate= options.defaultDate;
+
+  if(selectedDates[0]<newDate){
+    Notify.failure(`Please choose Date`,{
+      timeout: 2000,});
+  }else if (selectedDates[0]>newDate){
+    buttonStart.disabled=false;
+   timer=selectedDates[0];
+  }
+}
+function timerInterval(date){
+  intervalId=setInterval(()=>{
+    const currentTime=Date.now();
+    const countDownTime= date -currentTime;
+    const time =convertMs(countDownTime);
+    updateTime(time);
+    if(countDownTime<1000){
+    form.disabled=false;
+      clearInterval(intervalId);
+
+    }
+  },1000)
+}
+function onButton(){
+  buttonStart.disabled=true;
+  form.disabled=true;
+  timerInterval(timer)
+};
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');};
+
+  function convertMs(ms) {
+    const days = addLeadingZero(Math.floor((ms / (1000 * 60 * 60 * 24)) % 7));
+    const hours = addLeadingZero(Math.floor((ms / (1000 * 60 * 60)) % 24));
+    const minutes = addLeadingZero(Math.floor((ms / (1000 * 60)) % 60));
+    const seconds = addLeadingZero(Math.floor((ms / 1000) % 60));
+
+    return { days, hours, minutes, seconds};
+};
+  function updateTime({ days, hours, minutes, seconds })
+  { timeElAll[0].textContent=days;
+    timeElAll[1].textContent=hours;
+    timeElAll[2].textContent=minutes;
+    timeElAll[3].textContent=seconds;
+    }
+  
+    
+  
+
+
+
+
+
+
+  // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+  // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+  // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+ const form =document.querySelector('.form');
+ const nameDelay=document.querySelector('[name="delay"]');
+ const nameStep=document.querySelector('[name="step"]');
+ const nameAmount=document.querySelector('[name="amount"]')
+
+ form.addEventListener('submit',formSubmit)
+ console.log(form)
+ function formSubmit(e){
+  e.preventDefault();
+
+const delay=(nameDelay.value);
+const step=(nameStep.value);
+const amount=(nameAmount.value);
+ 
+for (let i=1;i<=amount;i+=1){
+  createPromise(i, delay)
+  .then(({ position, delay }) => Notify.success(`Fulfilled promise ${position} in ${delay}ms`))
+  .catch(({ position, delay }) => Notify.failure(`Rejected promise ${position} in ${delay}ms`))
+  delay +=step}
+ };
+
 function createPromise(position, delay) {
+ return new Promise((resolve,reject)=>{
+ setInterval(()=>{
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    resolve({position, delay} );
   } else {
-    // Reject
+    reject({position,delay});
   }
+ },delay);
 }
+ )}
+
+
